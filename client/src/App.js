17f64441a5b34fd70dc7218071d601b6ecc13159
samvrny <samvrny@gmail.com>
2022-10-26 19:57:04 -0500

    updated Appjs and Auth file (client side)

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-
-import Header from './components/Header';
+import { setContext } from '@apollo/client/link/context';
 import {
   ApolloClient,
   InMemoryCache,
@@ -11,40 +9,60 @@ import {
   createHttpLink,
 } from '@apollo/client';
 
+import Header from './components/Header';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    //<ApolloProvider client={client}>
-            <Router>
+    <ApolloProvider client={client}>
+      <Router>
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
             <Switch>
               <Route 
-                path="/" 
-                element={<Home />} 
+                exact path="/" 
+                component={Home} 
               />
               <Route 
-                path="/login" 
-                element={<Login />} 
+                exact path="/login" 
+                component={Login} 
               />
               <Route 
-                path="/signup" 
-                element={<Signup />} 
+                exact path="/signup" 
+                component={Signup} 
               />
               <Route 
-                path="/favorites" 
-                element={<Favorites />} 
+                exact path="/favorites" 
+                component={Favorites} 
               />
             </Switch>
           </div>
         </div>
       </Router>
-    //</ApolloProvider>
+    </ApolloProvider>
   );
 }
 
