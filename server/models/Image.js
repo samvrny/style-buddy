4273e0463cfb436d350470ff7e1e5fb382180ec4
Samuel Varney <105232555+samvrny@users.noreply.github.com>
2022-10-27 11:14:17 -0500

    Feature/sam (#17)
    * added server files
    
    * set up server
    
    * got the connection file written
    
    * set up the User model
    
    * created the mongoDB/mongoose models
    
    * initial files
    
    * trying to get compiling to work
    
    * trying to get compiling to work
    
    * updated Appjs and Auth file (client side)
    
    * added colors file
    
    * updated the palette and image models

--- server/models/Image.js
+++ server/models/Image.js
@@ -2,10 +2,31 @@ const { Schema } = require('mongoose');
 
 const ImageSchema = new Schema(
     {
-        image: {
+        id: {
             type: String,
-            unique: true
-        }
+            unique: true,
+            required: true
+        },
+        width: {
+            type: String,
+            required: true
+        },
+        height: {
+            type: String,
+            required: true
+        },
+        photographer: {
+            type: String,
+            required: true
+        },
+        src: {
+            type: String,
+            required: true
+        },
+        alt: {
+            type: String,
+            required: true
+        }   
     }
 );
 
