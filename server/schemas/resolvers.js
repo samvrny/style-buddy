06e42accdbb74ce18d2c978af183ab8acb9d30b1
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-10-26 19:03:59 -0500

    started typeDefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,31 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+    Query: {
+
+        me: async (parent, args, context) => {
+
+            if (context.user) {
+
+                const userData = await User.findOne({
+
+                    _id: context.user._id
+                })
+                .select('-__v -password')
+                .populate('palettes')
+                .populate('images')
+                .populate('fonts');
+
+                return userData;
+
+            }
+
+            throw new AuthenticationError('You\'re not logged in');
+        }
+    }
+}
+
+module.exports = resolvers;
