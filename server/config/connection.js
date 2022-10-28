259e69f59d33caa977d19585211e4ef42d8cf020
samvrny <samvrny@gmail.com>
2022-10-28 15:59:29 -0500

    fixed DB problems

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/style-buddies', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/style-friend', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
