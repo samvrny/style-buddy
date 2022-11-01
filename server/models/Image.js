8c7b41aa344244dd5a10752d66dd57323b36af85
Samuel Varney <105232555+samvrny@users.noreply.github.com>
2022-11-01 10:25:57 -0500

    Feature/sam (#35)
    * added server files
    
    * set up server
    
    * got the connection file written
    
    * set up the User model
    
    * created the mongoDB/mongoose models
    
    * initial files
    
    * trying to get compiling to work
    
    * trying to get compiling to work
    
    * updated Appjs and Auth file (client side)
    
    * added colors file
    
    * updated the palette and image models
    
    * got the random color generator working
    
    * changed database name
    
    * fixed DB problems
    
    * halfway with favorites page
    
    * committing
    
    * built the initial favorites page
    
    * finished with removing palettes and fonts
    
    * finished working on preventing duplicated data from going into the database functionality
    
    * added authorization
    
    * pushed up some minor changes

--- server/models/Image.js
+++ server/models/Image.js
@@ -6,19 +6,11 @@ const ImageSchema = new Schema(
             type: String,
             required: true
         },
-        width: {
-            type: String,
-            required: true
-        },
-        height: {
-            type: String,
-            required: true
-        },
         photographer: {
             type: String,
             required: true
         },
-        src: {
+        small: {
             type: String,
             required: true
         },
