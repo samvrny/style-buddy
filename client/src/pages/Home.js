26f3fbd2aab570d440c1544d3f7c9d759ee3c362
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-10-31 20:36:17 -0500

    pulled changes and adjusted package.json

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -80,11 +80,11 @@ const Home = () => {
         console.log(photoData, "Click")
         const photographer = photoData.photographer
         const small = photoData.photos[0].src.small
-        setSearchedImage({photographer: photographer, small: small})
+        setSearchedImage({ photographer: photographer, small: small })
         setSearchInput('')
     };
 
-    const handleSavePalette = async(randomizedPalette) => {
+    const handleSavePalette = async (randomizedPalette) => {
         const token = Auth.loggedIn() ? Auth.getToken() : null;
         console.log(token);
         if (!token) {
@@ -106,7 +106,7 @@ const Home = () => {
     const handleSaveFont = async (randomizedFont) => {
         //console.log(randomizedFont);
         const token = Auth.loggedIn() ? Auth.getToken() : null;
-        
+
         if (!token) {
             return false;
         }
@@ -146,38 +146,22 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+
             <div className="row">
-                <form id="image-search" className="col-lg-6">
-                    <div>
-                        <label htmlFor="search">Search Images:</label>
-                        <input type="text" defaultValue={searchInput} name="searchInput" />
-                    </div>
+                <form onSubmit={handlePhotoData} className="col-6 image-search">
                     <div>
                         <img src={searchedImage.small} alt="searched image"></img>
                     </div>
-                    <button type='submit' onClick={() => handlePhotoData()}>Submit</button>
+                    <div className="mx-2 mt-2">
+                        <input type="text" placeholder="Image Keyword" name="searchInput" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+                        <button className="mt-2 mx-2" type='submit'>Submit</button>
+                    </div>
                 </form>
-                <div className="font-box col-lg-6">
+                <div className="font-box col-6">
                     <div className="box" style={{ fontFamily: randomizedFont }}>{randomizedFont}</div>
+
                     <button onClick={() => handleRandomFont()}>Randomize!</button>
                     <button onClick={() => handleSaveFont(randomizedFont)}>TESTING FONT</button>
-                </div>
-            </div>
-        <div className="row">
-            <form onSubmit={handlePhotoData} className="col-6 image-search">
-                <div>
-                    <img src={searchedImage.small} alt="searched image"></img>
-                </div>
-                <div className="mx-2 mt-2">
-                    <input type="text" placeholder="Image Keyword" name="searchInput" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
-                    <button className="mt-2 mx-2" type='submit'>Submit</button>
-                </div>
-            </form>
-            <div className="font-box col-6">
-                <div className="box" style={{fontFamily: randomizedFont}}>{randomizedFont}</div>
-
-                <button onClick={() => handleRandomFont()}>Randomize!</button>
-                <button onClick={() => handleSaveFont(randomizedFont)}>TESTING FONT</button>
                 </div>
             </div>
         </>
