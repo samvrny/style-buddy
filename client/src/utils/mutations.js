6a8b7270a91e4bbb663b4703dfff526c4647e93b
kelsie kidd <kelsiekidd93@gmail.com>
2022-11-01 14:44:33 -0500

    added changes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,7 +25,7 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_IMAGE = gql`
-  mutation saveImage($imageId: String!, $photographer: String, $small: String, $alt: String) {
+  mutation saveImage($imageId: String, $photographer: String, $small: String, $alt: String) {
     saveImage(imageId: $imageId, photographer: $photographer, small: $small, alt: $alt) {
         savedImages {
             imageId
