89e7f98a8ed96d26f38e25f1843044477c8abe47
kelsie kidd <kelsiekidd93@gmail.com>
2022-11-02 11:31:24 -0500

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/style-friend', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:/style-friend', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
